cce13a53afeb4d9674fc8837d68baef73a175647
Haleema Tahir Malik <haleematahir277@gmail.com>
2025-05-07 01:32:50 +0500

    Terraform done

---
+++ Terraform/main.tf
@@ -0,0 +1,41 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("C:/Users/Haleema Tahir/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "django_sg" {
+  name        = "django_sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Only for dev/test
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "django_server" {
+  ami                         = "ami-028f6d7ddc3079baf"
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.django_sg.id]
+
+  tags = {
+    Name = "freelance-portal-instance"
+  }
+}
